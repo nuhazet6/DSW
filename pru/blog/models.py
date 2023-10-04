828cdb7e7158785ac7ed7942ad96f748065d1eb8
Nuhazet Correa Torres <nuhazet6@gmail.com>
2023-10-04 18:33:17 +0100

    Capítulo 1 terminado

--- pru/blog/models.py
+++ pru/blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
-    objects = models.Manage()
+    objects = models.Manager()
     published = PublishedManager()
 
     class Meta:
