7c99cc9869e3f2c2c17fe823e8d900bb19930363
Nuhazet Correa Torres <nuhazet6@gmail.com>
2023-10-23 14:11:51 +0100

    minor changes

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,16 +1,21 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import LoginForm
 
+
 # Create your views here.
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        if form.is_valid():  
+        if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
